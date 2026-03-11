d117b72d02fb37daf253a756876ab8d1ee8c315a
Samuel Krohn <sanikr@outlook.com>
2026-03-11 13:07:43 -0600

    Moving random files out

---
+++ Sandbox/Sandbox/Content/Maps/Modern Exterior Master Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Modern Exterior Master Tileset" tilewidth="32" tileheight="32" tilecount="90464" columns="176">
+ <image source="Modern Exterior Master Tileset 32x32.png" width="5632" height="16448"/>
+</tileset>
