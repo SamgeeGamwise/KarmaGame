0d7bb90084effe49540d7c7a570d71d7c74a6179
Samuel Krohn <sanikr@outlook.com>
2026-03-12 12:29:37 -0600

    Update map

--- Sandbox/Sandbox/Content/Maps/Modern Interior Master Tileset.tsx
+++ Sandbox/Sandbox/Content/Maps/Modern Interior Master Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Modern Interior Master Tileset" tilewidth="32" tileheight="32" tilecount="17024" columns="16">
- <image source="Modern Interior Master Tileset 32x32.png" width="512" height="34048"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Modern Interior Master Tileset 1" tilewidth="32" tileheight="32" tilecount="17024" columns="16">
+ <image source="Modern Interior Master Tileset 32x32 part 1.png" width="512" height="17024"/>
 </tileset>
